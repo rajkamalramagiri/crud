e087dc7779e7019637e0cf289d5ecb29ee8d24e0
Rajkamal <rajkamal.smit@gmail.com>
2021-06-06 17:51:42 +0530

    crud done

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv/config");
+app.use(express.json());
+
+app.use(cors());
 
 mongoose.connect(
   process.env.dbconnectionstring,
--- post.js
+++ post.js
@@ -1,11 +1,64 @@
 const express = require("express");
 const router = express.Router();
+const Posts = require("./models/post");
 
-router.get("/", (req, res) => {
-  res.send("we are on post");
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Posts.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const posts = await Posts.findById(req.params.id);
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const posts = await Posts.remove({ _id: req.params.id });
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const updatePost = await Posts.updateOne(
+      {
+        _id: req.params.id,
+      },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatePost);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 router.get("/spl", (req, res) => {
   res.send("we are on spl");
+});
+
+router.post("/", (req, res) => {
+  const post = new Posts({
+    title: req.body.title,
+    description: req.body.description,
+  });
+
+  post
+    .save()
+    .then((data) => res.json(data))
+    .catch((err) => {
+      res.json({ message: err });
+    });
+  res.send(req.body);
 });
 module.exports = router;
